bbd0dcde99209694c6cf11c47ac1127d064e5456
Helena Merk <helena.lmtm@gmail.com>
2021-11-29 01:27:44 +0200

    formula

---
+++ Formula/hello.rb
@@ -0,0 +1,10 @@
+class CCost < Formula
+  desc "Coinbase Coin Cost Match"
+  homepage "https://github.com/helenamerk/ccost"
+  url "https://github.com/helenamerk/homebrew-ccost/releases/download/v1.0.0/ccost-macos-x64.tar.gz"
+  sha256 "8032c9e87e8771f472f722ef875d0346959e30ec489feb01d5d6e7bf866924ea"
+  version "1.0.0"
+  def install
+    bin.install "ccost"
+  end
+end
