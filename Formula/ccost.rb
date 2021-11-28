c8e754e441d17ad9006b6fc3f038e296dbed5053
Helena Merk <helena.lmtm@gmail.com>
2021-11-29 01:39:58 +0200

    update hash

--- Formula/ccost.rb
+++ Formula/ccost.rb
@@ -1,8 +1,8 @@
 class Ccost < Formula
   desc "Coinbase Coin Cost Match"
   homepage "https://github.com/helenamerk/ccost"
-  url "https://github.com/helenamerk/homebrew-ccost/releases/download/v1.0.0/ccost-macos-x64.tar.gz"
-  sha256 "8032c9e87e8771f472f722ef875d0346959e30ec489feb01d5d6e7bf866924ea"
+  url "https://github.com/helenamerk/homebrew-ccost/releases/download/latest/ccost-macos-x64.tar.gz"
+  sha256 "738da2630be861205bf1f1106830f7186accf222c4b88606d6146f75fc57efe0"
   version "1.0.0"
   def install
     bin.install "ccost"
